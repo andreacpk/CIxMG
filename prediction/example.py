5834cbb87051e2c674691c4aa1dba8432dc6af13
andreacpk <deia.cpk@gmail.com>
2025-03-03 11:44:06 -0300

    Update example.py

--- prediction/example.py
+++ prediction/example.py
@@ -3,12 +3,11 @@
 """
 Created on Mon Mar  8 13:01:55 2021
 
-@author: rve
 """
 
 import sys
-import lbic_car
-import lbic_car_utils as utils
+import car
+import car_utils as utils
 import time
 import faulthandler
 
@@ -17,13 +16,13 @@ arg = utils.Arguments(sys.argv[1])
 
 print('evaluating biclusters')
 start_timeE = time.time()
-summary, bicsRowsClass = lbic_car.getInfoOptimalRulePerBic(arg.dtset.bics, arg.dtset.labels_train, attrWeights=None,
+summary, bicsRowsClass = car.getInfoOptimalRulePerBic(arg.dtset.bics, arg.dtset.labels_train, attrWeights=None,
                                                            attrCosts=None)
 end_timeE = time.time() - start_timeE
 
 print('runtime evaluating = ', end_timeE)
 print('n bics: ', len(summary))
-classCov = lbic_car.getClassCoverage(arg.dtset.labels_train, bicsRowsClass)
+classCov = car.getClassCoverage(arg.dtset.labels_train, bicsRowsClass)
 
-lbic_car.car_Coverage(arg, summary, bicsRowsClass, 1)
-lbic_car.car_Lazy(arg, summary, bicsRowsClass)
+car.car_Coverage(arg, summary, bicsRowsClass, 1)
+car.car_Lazy(arg, summary, bicsRowsClass)
